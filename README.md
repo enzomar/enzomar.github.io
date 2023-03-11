023d3ece2c4d15136fdab816110145fbc79d8fa0
Vincenzo MARAFIOTI <vincenzo.marafioti@amadeus.com>
2023-03-11 19:51:49 +0100

    removal of jinjia

--- README.md
+++ README.md
@@ -2,37 +2,8 @@
 
 # How generate a new portfolio
 
-## Nominal path
-
-1. Clone this repository
-
-2. Open and update the ```projects.csv```
-
-3. Commit and push the changes
-
-```bash 
-git commit -am'update' && git push
-```
-
-
-## Developer path 
-
-1. Clone this repository
-
-2. Open and update the ```projects.csv```
-
-3. Generate the new portfolio
-
-```bash 
-virtualenv -p python3 venv
-source venv/bin/activate
-pip3 install -r requirements.txt
-python3 generate.py
-```
-4. Open the index.html and verify
-
-5. Commit and push the changes
-
+1. Open and update the ```projects.js```
+2. Commit and push the changes
 ```bash 
 git commit -am'update' && git push
 ```
---
+++ projects.js
@@ -0,0 +1,64 @@
+var projects = {
+  "in_progress": [
+    {
+      "title": "pollicino",
+      "description": "The goal of the project is indeed to provide a way to automatize plants and garden caring and probably more",
+      "category": [
+        " iot",
+        "devops",
+        "cloud"
+      ],
+      "link": "https://github.com/enzomar/pollicino"
+    },
+    {
+      "title": "CQRS",
+      "description": "CQRS playground simulator; using docker compose of the main the standard building blocks of a CQRS architecture",
+      "category": [
+        " backend",
+        "devops",
+        "cloud"
+      ],
+      "link": "https://github.com/enzomar/cqrs"
+    }
+  ],
+  "done": [
+    {
+      "title": "KSELK",
+      "description": "Sandbox to play with Big Data.",
+      "category": [
+        " backend",
+        "devops",
+        "cloud"
+      ],
+      "link": "https://github.com/enzomar/KSELK"
+    },
+    {
+      "title": "Seamold",
+      "description": "My Cookiecutter template for python",
+      "category": [
+        " devops "
+      ],
+      "link": "https://github.com/enzomar/seamold"
+    },
+    {
+      "title": "OMB",
+      "description": "One Machine Band: The following repository contians a set of scripts and a tree structure to host a development; acceptance and production environment based on docker for web developments. The Production phase is capable of blue-green deploy",
+      "category": [
+        " backend",
+        "devops",
+        "cloud "
+      ],
+      "link": "https://github.com/enzomar/omb"
+    }
+  ],
+  "almost_done": [
+    {
+      "title": "Polly",
+      "description": "Small moking server. It acts as a proxy / caching. It is able to capture live traffic an replaying it afterwards.",
+      "category": [
+        " backend"
+      ],
+      "link": "https://github.com/enzomar/Polly"
+    }
+  ]
+}
