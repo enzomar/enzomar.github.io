ef5d6599a9ba2bd1ef31330d9af2ee0b865c806d
Vincenzo MARAFIOTI <vincenzo.marafioti@amadeus.com>
2023-03-11 20:04:21 +0100

    update

--- README.md
+++ README.md
@@ -2,8 +2,12 @@
 
 # How generate a new portfolio
 
-1. Open and update the ```projects.js```
-2. Commit and push the changes
+1. Update the ```projects.csv```
+2. Transform the csv
+```bash 
+python generate.py
+```
+3. Commit and push the changes
 ```bash 
 git commit -am'update' && git push
 ```
--- generate.py
+++ generate.py
@@ -1,14 +1,8 @@
 import os
-from jinja2 import Environment, FileSystemLoader
 import csv
-from datetime import datetime
-
-# Create the jinja2 environment.
-current_directory = os.path.dirname(os.path.abspath(__file__))
-env = Environment(loader=FileSystemLoader(current_directory))
+import json
 
 STATUS = ['idea', 'in_progress', 'almost_done', 'done']
-CATEGORIES = ['backend', 'frontend', 'db', 'devops', 'iot', 'hardware', 'bestpractices', 'cloud']
 
 def load_projects():
     projects = {}
@@ -18,7 +12,6 @@ def load_projects():
         # TITLE, DESCRIPTION, STATUS, CATEGORIES, LINK
 
         for row in reader:
-            # process each row
             title = row[0].strip()
             description = row[1].strip()
             status = row[2].strip()
@@ -29,32 +22,22 @@ def load_projects():
                 projects[status] = []
             projects[status].append({"title": title, 
                 "description": description, 
-                "category":category, 
+                "category":[s.strip() for s in category], 
                 "link":link })
-
-    projects['date'] = datetime.now().isoformat("T")
 
     return projects
 
 
 
-def run(template_name, projects):
-    current_directory = os.path.dirname(os.path.abspath(__file__))
-    env = Environment(loader=FileSystemLoader(current_directory))
-    template = env.get_template(template_name)
-
-    html = template.render(projects)
-
-    with open('index.html', 'w') as outfile:
-        outfile.write(html)
-
+def run(projects):
+   
+    with open('projects.js', 'w') as outfile:
+        outfile.write("var projects ={0}".format(json.dumps(projects, indent=4)))
 
 
 if __name__ == "__main__":
     projects = load_projects()
-    print(projects)
-    
-    run('template.j2', projects)
+    run(projects)
 
 
 
