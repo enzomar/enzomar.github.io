8d54c8ecc7c79c38dcc2541497f6a9833c80525b
Vincenzo MARAFIOTI <vincenzo.marafioti@amadeus.com>
2023-03-13 11:18:08 +0100

    test pre commit-hooks

--- bump.sh
+++ bump.sh
@@ -6,6 +6,6 @@ timestamp=$(date +"%Y-%m-%dT%H:%M:%S%z")
 
 echo -n "var version = \"" > version.js
 echo -n $changeset >> version.js
-echo -n "@" >> version.js
+echo -n "#" >> version.js
 echo -n $timestamp >> version.js
 echo -n "\";" >> version.js
