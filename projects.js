030a1bd042522146c54887bff72462b39d731eb6
Vincenzo MARAFIOTI <vincenzo.marafioti@amadeus.com>
2023-03-13 20:26:08 +0100

    update

--- projects.js
+++ projects.js
@@ -268,7 +268,7 @@ var projects ={
         },
         {
             "title": "Fleeo",
-            "description": "Fleoo is a pay-per-view streaming platform that offers to content creator a way to transmit their live show via 3rd parties streaming solution ( ie. Zoom).",
+            "description": "Fleoo is a pay-per-view streaming platform that offers to content creator a way to transmit their live show via 3rd parties streaming solutions.",
             "size": "XL",
             "category": [
                 "backend",
